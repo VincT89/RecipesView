92089731fbfd173a5773515e07f6e1c184dc677a
VincT89 <vincenzotito@icloud.com>
2025-10-20 13:42:35 +0200

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { createRoot } from 'react-dom/client'
+import { Provider as ReduxProvider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-    <App />
+    <ReduxProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ReduxProvider>
 
-)
+);
