222cdb3a30ae69b09d39c89b8033a98a7170ff6f
Tonyx8808 <antruss080888@gmail.com>
2025-10-21 14:41:19 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import RecipesList from './components/RecipesList';
+
+function App() {
+  return (
+    <div>
+      <h1>Ricette</h1>
+      <RecipesList />
+    </div>
+  );
+}
+
+export default App;
